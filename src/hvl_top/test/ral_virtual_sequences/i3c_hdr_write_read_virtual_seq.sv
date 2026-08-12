fcd736fc531673d9ec91d8ff01035913c826899a
Shama M <shama@feserver.mirafra.com>
2026-08-12 15:32:15 +0530

    hdr

--- src/hvl_top/test/ral_virtual_sequences/i3c_hdr_write_read_virtual_seq.sv
+++ src/hvl_top/test/ral_virtual_sequences/i3c_hdr_write_read_virtual_seq.sv
@@ -85,6 +85,9 @@ else
       end
     join_none
 
+ i3c_env_cfg_h.i3c_target_agent_cfg_h[0].pending_hdr_write = 1;
+
+
     // Step 1b: load TX FIFO via WDATAB
     `uvm_info(get_type_name(), "Step 1: Loading TX FIFO (WDATAB)", UVM_LOW)
     foreach (wdata[i]) begin
@@ -136,6 +139,8 @@ i3c_env_cfg_h.regBlockHandle.ctrl_inst.start.set(1'b1);
         tgt_read.start(p_sequencer.i3c_target_seqr_h[0]);
       end
     join_none
+ i3c_env_cfg_h.i3c_target_agent_cfg_h[0].pending_hdr_read = 1; 
+
 
     // Step 2b: write CTRL — HDR READ
     `uvm_info(get_type_name(),
